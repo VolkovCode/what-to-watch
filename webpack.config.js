e802fe479be3105e9721fab8283d9d49be70962f
Волков Алексей <wolf.alex.1997@mail.ru>
2022-09-04 18:44:06 +0300

    Пробую деплоить vercel

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,6 @@ module.exports = {
     ],
   },
   resolve: {
-    modules: [resolve(process.cwd(), 'src'), 'node_modules'],
     extensions: ['*', '.js', '.jsx', '.json'],
     symlinks: false,
     cacheWithContext: false
