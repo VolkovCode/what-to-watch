c48feea5503eb560159b020c8a22028ca6afee03
Волков Алексей <wolf.alex.1997@mail.ru>
2022-09-05 16:14:08 +0300

    vercel?

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {reducer} from './store/reducer';
 import {createAPI} from './api/api';
 import {ActionCreator} from './store/action';
 import {AuthorizationStatus} from './data/constants';
-import App from './components/app/app';
+// import App from './components/app/app';
 
 
 const api = createAPI(
@@ -28,5 +28,5 @@ const store = createStore(
 const root = ReactDOM.createRoot(document.getElementById(`root`));
 root.render(
     <Provider store={store}>
-      <App/>
+      {/* <App/> */}
     </Provider>);
