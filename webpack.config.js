e6b7f896acdb63b0dc57fba418000be0eed0c0ff
Волков Алексей <wolf.alex.1997@mail.ru>
2022-09-05 15:13:22 +0300

    gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
